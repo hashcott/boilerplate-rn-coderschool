a5d4cf11b804c7dfb97d35fad4ccf2a925940758
Nguyen Duc Hanh <1851061469@e.tlu.edu.vn>
2020-09-10 14:03:57 +0700

    add schedule

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import Home from "./containers/Home";
 
@@ -13,7 +15,26 @@ const Drawer = createDrawerNavigator();
 const Router = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "schedule" : "schedule";
+            } else if (route.name === "Settings") {
+              iconName = focused ? "ios-list-box" : "ios-list";
+            }
+
+            // You can return any component that you like here!
+            return <MaterialIcons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "#00A4EF",
+          inactiveTintColor: "gray",
+        }}
+      >
         <Tab.Screen name="Home" component={Home} />
       </Tab.Navigator>
     </NavigationContainer>
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,20 +1,111 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { createExample } from "../actions/Example";
-import { connect } from "react-redux";
+import React, { Component } from "react";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import { Agenda } from "react-native-calendars";
+import { AntDesign } from "@expo/vector-icons";
 
-const Home = () => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home</Text>
-    </View>
-  );
-};
+const dummy = require("../apis/dummy.json");
 
-const mapStateToProps = (state) => {
-  return { ...state.example };
-};
+export default class AgendaScreen extends Component {
+  componentDidMount() {
+    this.setState({ items: dummy.timeTable });
+  }
+  constructor(props) {
+    super(props);
 
-export default connect(mapStateToProps, { createExample })(Home);
+    this.state = {
+      items: {},
+    };
+  }
 
-const styles = StyleSheet.create({});
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <Agenda
+          items={this.state.items}
+          // loadItemsForMonth={this.loadItems.bind(this)}
+          selected={this.timeToString(new Date().getTime())}
+          renderItem={this.renderItem.bind(this)}
+          renderEmptyDate={this.renderEmptyDate.bind(this)}
+          rowHasChanged={this.rowHasChanged.bind(this)}
+        />
+      </SafeAreaView>
+    );
+  }
+
+  renderItem(item) {
+    return (
+      <TouchableOpacity
+        style={styles.item}
+        onPress={() => Alert.alert(item.lopHocPhan)}
+      >
+        <View style={styles.title}>
+          <Text style={{ fontWeight: "bold" }}>{item.lopHocPhan}</Text>
+          <Text
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <AntDesign name="clockcircleo" size={18} color="black" />{" "}
+            {this.parsePeriods(item.timestamp.start)} -{" "}
+            {this.parsePeriods(item.timestamp.end)}
+          </Text>
+          <Text>{item.location}</Text>
+          <Text>{item.giangVien}</Text>
+          <Text>Số tín chỉ : {item.soTc}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>Bạn chưa có lịch học vào hôm nay</Text>
+      </View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split("T")[0];
+  }
+  parsePeriods(time) {
+    const date = new Date(time);
+    return `${date.getHours()}:${date.getMinutes()}`;
+  }
+}
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "white",
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17,
+    height: 150,
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30,
+  },
+  title: {
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+  },
+});
